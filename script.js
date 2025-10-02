944f2dc46a66b35acad1818c046a80747b2ea7a8
manikanta639 <manikantanagubandi9@gmail.com>
2025-10-02 21:35:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Sample Product Data
 const products = [
     { 
+        id: 4, 
+        name: "Striped Shirt", 
+        category: "shirts", 
+        image: "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/2025/MAY/7/YPZwCB9L_6ce665d1fdb343f388127e471c5bdb09.jpg", 
+        link: "" 
+    },
+    { 
         id: 3, 
         name: "Sneakers", 
         category: "combos", 
@@ -104,6 +111,28 @@ const comboProducts = {
     image: "https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/l/f/2/-original-imahgbrvwfjpg9fh.jpeg?q=70&crop=false",
     link: "https://fkrt.co/yOP03n"
   }
+],
+4: [
+  {
+    name: "Shirt 1",
+    image: "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/2025/MAY/7/XDhBEReQ_8ce024c24b054112852f9c9e25706a39.jpg",
+    link: "https://myntr.in/ZBTaHr"
+  },
+  {
+    name: "Shirt 2",
+    image: "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/2025/MAY/7/iETIOqml_093de7982597463cb30609ae0c945f3c.jpg",
+    link: "https://myntr.in/K0Llim"
+  },
+    {
+    name: "Shirt 3",
+    image: "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/33988086/2025/5/16/3da1f93d-3955-43f7-b3de-5f780e778a6a1747373637174-HERENOW-Men-Tartan-Checks-Casual-Shirt-8391747373636541-1.jpg",
+    link: "https://myntr.in/fNhBln"
+  },
+    {
+    name: "Shirt 4",
+    image: "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/33203161/2025/3/20/47df1e5d-2cb3-45de-9050-a72cbfaa097c1742480051691-HERENOW-Men-Opaque-Striped-Casual-Shirt-5041742480051024-4.jpg",
+    link: "https://myntr.in/DY8GYl"
+  },
 ],
 /*-------------------------------------------------------------------------------------------------------------*/    
 /*7: [
@@ -276,6 +305,7 @@ window.onload = function() {
     renderProducts(products);
     document.body.style.opacity = 1; // ensure body visible initially
 };
+
 
 
 
